431a0cf103cc20b3dc529705fac74fe4eed6d29b
TheHumbleJester <the.humble.jester@gmail.com>
2020-06-15 21:44:08 +0200

    feat: add codebase (#1)

---
+++ integration-tests/main-scenario/index.test.ts
@@ -0,0 +1,91 @@
+import fs from 'fs';
+import { Sandbox } from 'alliage-sandbox';
+
+describe('Main scenario', () => {
+  const sandbox = new Sandbox({
+    scenarioPath: __dirname,
+  });
+
+  beforeAll(async () => {
+    await sandbox.init();
+    await sandbox.install(['alliage-core']).waitCompletion();
+  });
+
+  afterAll(async () => {
+    await sandbox.clear();
+  });
+
+  it('should install correctly and create the tsconfig.json file', async () => {
+    const { waitCompletion } = await sandbox.install(['alliage-typescript']);
+
+    await waitCompletion();
+
+    const tsConfigFile = `${sandbox.getPath()}/tsconfig.json`;
+    expect(fs.existsSync(tsConfigFile)).toBe(true);
+    expect(fs.readFileSync(tsConfigFile).toString()).toMatchSnapshot();
+
+    expect(
+      JSON.parse(fs.readFileSync(`${sandbox.getPath()}/alliage-modules.json`).toString()),
+    ).toEqual(
+      expect.objectContaining({
+        'alliage-typescript': {
+          deps: [
+            'alliage-lifecycle',
+            'alliage-di',
+            'alliage-config-loader',
+            'alliage-module-installer',
+            'alliage-builder',
+          ],
+          hash: 'a19a4abc235e6afa08c548931e18158d',
+          module: 'alliage-typescript',
+        },
+      }),
+    );
+  });
+
+  it('should allow to execute the command with ts-node', async () => {
+    const { waitCompletion, process: childProcess } = sandbox.run([
+      'dummy-process',
+      'test',
+      '--use-typescript',
+      '--ts-services-basepath=src',
+      '--ts-project=./tsconfig.json',
+    ]);
+
+    let output = '';
+    childProcess.stdout!.on('data', (chunk) => {
+      output += chunk;
+    });
+    await waitCompletion();
+    expect(output).toEqual(
+      'Hello Alliage TypeScript ! - test - production\nabout to shut down...\nshutting down with signal: @process-manager/SIGNAL/SUCCESS_SHUTDOWN\n',
+    );
+  });
+
+  it('should compile the TypeScript code and run the compiled version', async () => {
+    const { waitCompletion: waitBuildCompletion, process: buildProcess } = sandbox.build([]);
+    buildProcess.stderr!.pipe(process.stderr);
+    buildProcess.stdout!.pipe(process.stdout);
+
+    await waitBuildCompletion();
+
+    // Removing the source directory should ensure that it's the compiled version that is actually  executed
+    fs.rmdirSync(`${sandbox.getPath()}/src`, { recursive: true });
+
+    const { waitCompletion: waitRunCompletion, process: runProcess } = sandbox.run([
+      'dummy-process',
+      'test',
+    ]);
+    runProcess.stderr!.pipe(process.stderr);
+
+    let output = '';
+    runProcess.stdout!.on('data', (chunk) => {
+      output += chunk;
+    });
+
+    await waitRunCompletion();
+    expect(output).toEqual(
+      'Hello Alliage TypeScript ! - test - production\nabout to shut down...\nshutting down with signal: @process-manager/SIGNAL/SUCCESS_SHUTDOWN\n',
+    );
+  });
+});
